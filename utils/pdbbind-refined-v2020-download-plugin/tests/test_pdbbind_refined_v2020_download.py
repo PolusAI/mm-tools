36d8979c94203f8112851dde0e7d7b0fa4807f0e
Brandon Duane Walker <walkerbd@UPDATEME.HOSTNAME.COM>
2024-03-18 14:54:35 -0400

    extract pdbbind refined from container

---
+++ utils/pdbbind-refined-v2020-download-plugin/tests/test_pdbbind_refined_v2020_download.py
@@ -0,0 +1,9 @@
+"""Tests for pdbbind_refined_v2020_download."""
+
+
+def test_pdbbind_refined_v2020_download() -> None:
+    """Test pdbbind_refined_v2020_download."""
+    # currently no way to not have a test on github workflow
+    # also since docker build happens after test cannot test
+    # via docker copy to see if data can be downloaded
+    assert True
