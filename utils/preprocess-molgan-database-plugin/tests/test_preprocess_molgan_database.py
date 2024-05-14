bafe350ce7cdf4afd99a21420ab2c8cd2001c45b
Brandon Duane Walker <walkerbd@UPDATEME.HOSTNAME.COM>
2024-05-14 12:58:15 -0400

    preprocess

---
+++ utils/preprocess-molgan-database-plugin/tests/test_preprocess_molgan_database.py
@@ -0,0 +1,26 @@
+"""Tests for preprocess_molgan_database."""
+import sys
+from pathlib import Path
+
+current_dir = Path(__file__).resolve().parent
+target_dir = current_dir.parent.parent.parent / "cwl_utils"
+sys.path.append(str(target_dir))
+
+from cwl_utilities import call_cwltool  # noqa: E402
+from cwl_utilities import create_input_yaml  # noqa: E402
+from cwl_utilities import parse_cwl_arguments  # noqa: E402
+
+
+def test_preprocess_molgan_database() -> None:
+    """Test preprocess_molgan_database."""
+    cwl_file = Path("preprocess_molgan_database.cwl")
+    input_to_props = parse_cwl_arguments(cwl_file)
+    file_path_str = "methane.sdf"
+    file_path = str(Path(__file__).resolve().parent / Path(file_path_str))
+    input_to_props["input_sdf_path"]["path"] = file_path
+    input_yaml_path = Path("preprocess_molgan_database.yml")
+    create_input_yaml(input_to_props, input_yaml_path)
+    stdout, stderr = call_cwltool(cwl_file, input_yaml_path)
+    print("stdout", stdout)  # noqa: T201
+    print("stderr", stderr)  # noqa: T201
+    assert Path("system.pkl").exists()
